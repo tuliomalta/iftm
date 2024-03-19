4b7701c68bc97b43d9f452a94e4889b04f982735
Tulio Malta <tuliomalta@live.com>
2024-03-18 22:15:42 -0300

    Commit da lista03

--- logicadeprogramacao/lista02/src/Media3.java
+++ logicadeprogramacao/lista02/src/Media3.java
@@ -7,7 +7,7 @@ public class Media3 {
 
         Scanner s = new Scanner(System.in);
 
-        float n1, n2, n3, n4, notaExame;
+        float n1, n2, n3, n4;
 
         n1 = s.nextFloat();
         n2 = s.nextFloat();
@@ -18,26 +18,29 @@ public class Media3 {
         float media = ((n1 * 2) + (n2 * 3) + (n3 * 4) + (n4 * 1)) / 10;
 
         if (media >= 7.0) {
-            System.out.println("Aluno aprovado");
+            System.out.printf("Media: %.1f\n", media);
+            System.out.println("Aluno aprovado.");
         } else if (media < 5.0) {
-            System.out.println("Aluno reprovado");
-        } else if (media >= 5.0 || media <= 6.9) {
-            System.out.println("Aluno em exame");
-        }
-
-        notaExame = s.nextFloat();
-
-        System.out.printf("Nota do exame: %.1f\n", notaExame);
-
-        float novaMedia = (media + notaExame) / 2;
-
-        if (novaMedia >= 5.0) {
-            System.out.println("Aluno aprovado");
+            System.out.printf("Media: %.1f\n", media);
+            System.out.println("Aluno reprovado.");
         } else {
-            System.out.println("Aluno reprovado");
-        }
+            System.out.printf("Media: %.1f\n", media);
+            System.out.println("Aluno em exame.");
 
-        System.out.printf("Media final: %.1f\n", novaMedia);
+            float notaExame = s.nextFloat();
+
+            System.out.printf("Nota do exame: %.1f\n", notaExame);
+
+            float novaMedia = (media + notaExame) / 2;
+
+            if (novaMedia >= 5.0) {
+                System.out.println("Aluno aprovado.");
+            } else {
+                System.out.println("Aluno reprovado.");
+            }
+
+            System.out.printf("Media final: %.1f\n", novaMedia);
+        }
 
         s.close();
     }
