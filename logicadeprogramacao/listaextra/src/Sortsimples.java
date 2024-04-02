7e743e633b697994dc3aee56a04c6196b81359f5
Tulio Malta <tuliomalta@live.com>
2024-04-02 01:31:45 -0300

    add lista 07 de PWE e add pasta de lista extra de LP

---
+++ logicadeprogramacao/listaextra/src/Sortsimples.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Sortsimples {
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+
+        int a;
+        int b;
+        int c;
+
+        
+
+    }
+}
