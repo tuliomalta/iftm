865bdef69ac961bfe427a611391580d15dbe85a1
Tulio Malta <tuliomalta@live.com>
2024-07-10 22:42:43 -0300

    att LP

--- logicadeprogramacao/lista04/src/SomaDeImparesConsecutivos1.java
+++ logicadeprogramacao/lista04/src/SomaDeImparesConsecutivos1.java
@@ -8,7 +8,21 @@ public class SomaDeImparesConsecutivos1 {
         int x = s.nextInt();
         int y = s.nextInt();
 
+        int start = x;
+        int end = y;
 
+        if (x > y) {
+            start = y;
+            end = x;
+        }
 
+        int soma = 0;
+
+        for (int i = start + 1; i < end; i++) {
+            if (i % 2 != 0) {
+                soma += i;
+            }
+        }
+        System.out.println(soma);
     }
 }
