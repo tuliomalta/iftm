cb841f33166562ca08ea41b0534306de6a0d9205
Tulio Malta <tuliomalta@live.com>
2024-07-25 00:40:24 -0300

    novos arquivos

---
+++ logicadeprogramacao/lista04/src/Intervalo2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Intervalo2 {
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+
+        int x = s.nextInt();
+        int y = s.nextInt();
+
+        
+
+
+    }
+}
