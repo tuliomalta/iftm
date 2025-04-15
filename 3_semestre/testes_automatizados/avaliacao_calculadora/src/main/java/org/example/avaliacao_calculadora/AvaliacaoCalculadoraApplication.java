b9c604bbef6de7e8fe9dba8c9cac78593284bd57
Tulio Malta <tuliomalta@live.com>
2025-04-14 21:51:50 -0300

    avaliação testes

---
+++ 3_semestre/testes_automatizados/avaliacao_calculadora/src/main/java/org/example/avaliacao_calculadora/AvaliacaoCalculadoraApplication.java
@@ -0,0 +1,13 @@
+package org.example.avaliacao_calculadora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvaliacaoCalculadoraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AvaliacaoCalculadoraApplication.class, args);
+    }
+
+}
---
+++ 3_semestre/testes_automatizados/avaliacao_calculadora/src/test/java/org/example/avaliacao_calculadora/AvaliacaoCalculadoraApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.avaliacao_calculadora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvaliacaoCalculadoraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
