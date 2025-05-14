ed595f1d2f265f4332c85d57ab7f7278bb13c78e
Tulio Malta <tuliomalta@live.com>
2025-05-13 21:59:19 -0300

    Prova01 - POO2

--- 3_semestre/POO2/provas/prova01/src/main/java/org/example/prova01/Transacao.java
+++ 3_semestre/POO2/provas/prova01/src/main/java/org/example/prova01/Transacao.java
@@ -1,7 +1,9 @@
 package org.example.prova01;
 
 public abstract class Transacao implements Operacao {
+
     protected Produto produto;
+
     protected int quantidade;
 
     public Transacao(Produto produto, int quantidade) {
--- 3_semestre/POO2/provas/prova01/src/main/java/org/example/prova01/Venda.java
+++ 3_semestre/POO2/provas/prova01/src/main/java/org/example/prova01/Venda.java
@@ -7,10 +7,13 @@ public class Venda extends Transacao {
 
     @Override
     public String executar() throws Exception {
+
         if (produto.getQuantidade() < quantidade) {
             throw new Exception("Estoque insuficiente para venda");
         }
+
         produto.atualizarEstoque(-quantidade);
+
         String msg = "Venda Realizada: " + quantidade + " X " + produto.getNome();
         GerenciaEstoque.registrarTransacao(msg);
         return msg;
